08487752f27602a9c53a1c32a2f8af36ba5abe97
PaulSouthern <paul.southern@sage.com>
2018-09-26 08:16:36 +0100

    Removing unused class

--- page_objects/booking_form.rb
+++ page_objects/booking_form.rb
@@ -32,25 +32,3 @@ class BookingForm
     @driver.find_element(css: "input[onclick='#{booking_id}']").click
   end
 end
-
-class Login
-
-  LOGIN_BUTTON = { class: 'login' }
-  EMAIL_INPUT = { id: 'email' }
-  PASSWORD_INPUT = { id: 'passwd' }
-  SUBMIT_BUTTON = { id: 'SubmitLogin' }
-
-  def initialize(driver)
-    @driver = driver
-    @driver.get 'http://automationpractice.com'
-  end
-
-  def with(username, password)
-    # Sign In
-    @driver.find_element(LOGIN_BUTTON).click
-    @driver.find_element(EMAIL_INPUT).send_keys(username)
-    @driver.find_element(PASSWORD_INPUT).send_keys(password)
-    @driver.find_element(SUBMIT_BUTTON).click
-  end
-end
-
